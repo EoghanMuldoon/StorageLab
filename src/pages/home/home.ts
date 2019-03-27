edf4fb10fb868128436302e44b18126e4556c4c6
Éoghan Muldoon <37183734+EoghanMuldoon@users.noreply.github.com>
2019-03-27 15:33:59 +0000

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  information:string;
+  constructor(public navCtrl: NavController, private storage: Storage) {
 
   }
-
+ 
+ 
+  OpenStatusPage(){
+    this.navCtrl.push("StatusPage");
+    
+  }
+  ionViewWillEnter(){
+   this.storage.get("Status")
+   .then((val)=>{
+    this.information=val;
+   }).catch((err)=> {
+     console.log(err);
+   });
+  }
 }
